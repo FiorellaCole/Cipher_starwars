9515f685efe125acbea023881618cf1c1443b589
FiorellaCole <fiorellacoleaguilar@gmail.com>
2022-06-09 23:45:29 -0500

    cambio final

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,49 @@
 const cipher = {
 
-  encode: function (texto, offset){
+    encode: (offset, string) => {
+        let resultado="";
+        let mensaje="";
+        offset=parseInt(offset);
+      if(string !="" && offset !="" && string != " " && offset != 0){
+        for(let i=0; i<string.length;i++){
+          let posicion = string.charCodeAt(i);         
+          if (posicion >= 65 && posicion <= 90) {
+            mensaje = ((posicion - 65 + offset) % 26) + 65;   // Mayusculas
+          } else if (posicion >= 97 && posicion <= 122) {  // Minusculas
+            mensaje = ((posicion - 97 + offset) % 26) + 97;
+          }else if (posicion === 32) {  // que lea espacios
+            mensaje = 32;
+          }
+          resultado += String.fromCharCode(mensaje);
+        }    
+        return resultado;
+      }else{
+        throw new TypeError("Valores no aceptados");
+      }
+    },
+    decode: (offset, string) => {
+      let desencriptado = "";
+      let mensajeDesencriptado = "";
+      offset= parseInt(offset);
 
-    let formula;
-    let codigoEnAscii = "" ;
-    let cifrarMayuscula = "" ;
-    let cifrarMinuscula= "" ;
-    offset = parseInt(offset);
-
-    if(texto === texto.toUpperCase()) {
-        for (let i = 0; i <= texto.length ; i++){
-
-            
+    if(string !="" && offset !="" && string != " " && offset != 0){
+      for (let i = 0; i < string.length; i++){
+        let posicion = string.charCodeAt(i);   
+        if (posicion >= 65 && posicion <= 90) {  // mayusculas
+          mensajeDesencriptado = ((posicion - 90 - offset) % 26) + 90;
+        }else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {     //Fórmula letras minúsculas
+          mensajeDesencriptado = ((posicion - 122 - offset) % 26) + 122;
+        }else if (posicion === 32) {       //Espacio
+          mensajeDesencriptado = 32;
         }
-
-    } 
-
-
- 
-
-    return "hola";
-  
-  }
-      
-        
-      
-};
+        desencriptado += String.fromCharCode(mensajeDesencriptado);
+      } 
+      return desencriptado;
+    }else{
+      throw new TypeError();
+    }  
+ }
+}
 
 export default cipher;
 
--- src/index.js
+++ src/index.js
@@ -1,48 +1,54 @@
 import cipher from './cipher.js';
 
 const botonCifrado = document.getElementById("cifrar");
-  botonCifrado.addEventListener("click", cifrar);
+botonCifrado.addEventListener("click", cifrar);
 
 
-/*const botonDescifrado = document.getElementById("descifrar");
-  botonCifrado.addEventListener("click", descifrar);
-
-  */
+const botonDescifrado = document.getElementById("descifrar");
+botonDescifrado.addEventListener("click", descifrar);
 
 
-document.querySelector("#siguiente").addEventListener("click", ()=>{    
+document.querySelector("#siguiente").addEventListener("click", () => {
   document.querySelector("#pantalla1").style.display = "none";
-  document.querySelector("#pantalla2").style.display= "block";
+  document.querySelector("#pantalla2").style.display = "block";
 });
 
 
-document.querySelector("#empezar").addEventListener("click", ()=>{   
-    document.querySelector("#pantalla2").style.display = "none";
-    document.querySelector("#pantalla3").style.display= "block";   
-  });
+document.querySelector("#empezar").addEventListener("click", () => {
+  document.querySelector("#pantalla2").style.display = "none";
+  document.querySelector("#pantalla3").style.display = "block";
+});
 
 
-function validationoffSet(){
-  return  document.getElementById("offset").value == "" ? alert ("Ingresa los valores") : document.getElementById("offset").value ;
+function validationoffSet() {
+  return document.getElementById("offset").value == "" ? alert("Ingresa los valores") : document.getElementById("offset").value;
 }
 
-function validartextoCifrado(){
-  return  document.getElementById("mensaje").value == "" ? alert ("Ingresa el texto a cifrar") : document.getElementById("mensaje").value;
+function validartextoCifrado() {
+  return document.getElementById("mensaje").value == "" ? alert("Ingresa el texto a cifrar") : document.getElementById("mensaje").value;
 }
 
 
-function cifrar (){
+function cifrar() {
   validationoffSet()
   validartextoCifrado()
   const mensajeACifrar = document.getElementById("mensaje").value;
   const offset = document.getElementById("offset").value;
-  document.getElementById("mensaje").innerHTML = cipher.encode(offset, mensajeACifrar);
+  document.getElementById("resultado").innerHTML = cipher.encode(offset, mensajeACifrar);
 
+}
+
+function descifrar() {
+  validationoffSet()
+  validartextoCifrado()
+  const mensajeADescifrar = document.getElementById("mensaje").value;
+  const offset = document.getElementById("offset").value;
+  document.getElementById("resultado").innerHTML = cipher.decode(offset, mensajeADescifrar);
 
 }
 
 
-  
 
-  
+
+
 
